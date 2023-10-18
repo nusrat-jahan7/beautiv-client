6320cf85bc18e34d16aaede968028d55afb58dba
nusrat-jahan7 <nusrat.jahan03060@gmail.com>
2023-10-18 17:36:04 +0600

    firebase Authentiction added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,7 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import logo from "/public/logo.png";
 import { useContext } from "react";
-import { AuthContext } from "../context/AuthProvider";
 import toast from "react-hot-toast";
+import { AuthContext } from "../context/AuthProvider";
 import { formatFirebaseAuthErrorMessage } from "../helpers";
 
 const Login = () => {
@@ -51,14 +50,16 @@ const Login = () => {
   };
 
   return (
-    <div className="bg-gray-100 py-20">
+    <div className="bg-gradient-to-r from-pink-400 via-purple-300 to-pink-400 py-20">
       <div className="w-full max-w-sm p-6 m-auto mx-auto bg-white font-poppins rounded-lg shadow-lg">
         <div className="flex justify-center mx-auto">
-          <img className="w-auto h-7 sm:h-8" src={logo} alt="" />
+        <h1 className="text-4xl text-pink-600 font-bold text-center mt-4">
+           Welcome Back!
+          </h1>
         </div>
 
         <div>
-          <h1 className="text-2xl text-cyan-700 font-semibold text-center mt-4">
+          <h1 className="text-3xl text-pink-600 font-bold text-center mt-4">
             LOGIN
           </h1>
         </div>
@@ -89,7 +90,7 @@ const Login = () => {
               name="password"
               id="password"
               placeholder="•••••••••"
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
 
@@ -97,7 +98,7 @@ const Login = () => {
             <button
             disabled={loading}
               type="submit"
-              className="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-cyan-700 rounded-lg hover:bg-cyan-900 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50 disabled:bg-cyan-700/50"
+              className="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-pink-700 rounded-lg hover:bg-purple-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50 disabled:bg-purple-700/50"
             >
               {
                 loading ? "Loading" : "Login"
@@ -124,7 +125,7 @@ const Login = () => {
           <button
             onClick={() => handleGoogleSignIn()}
             type="submit"
-            className="flex items-center justify-center w-full px-6 py-2 mx-2 text-sm font-medium text-white transition-colors duration-300 transform bg-cyan-700 rounded-lg hover:bg-cyan-900 focus:bg-cyan-700 focus:outline-none"
+            className="flex items-center justify-center w-full px-6 py-2 mx-2 text-sm font-medium text-white transition-colors duration-300 transform bg-pink-700 rounded-lg hover:bg-purple-700 focus:bg-blue-700 focus:outline-none"
           >
             <svg className="w-4 h-4 mx-2 fill-current" viewBox="0 0 24 24">
               <path d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z"></path>
@@ -139,7 +140,7 @@ const Login = () => {
           Do not have an account?{" "}
           <Link
             to="/register"
-            className="font-medium text-cyan-700 hover:underline"
+            className="font-medium text-pink-700 hover:underline"
           >
             Register
           </Link>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,170 @@
+import { useContext, useState } from "react";
+import { AuthContext } from "../context/AuthProvider";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import { formatFirebaseAuthErrorMessage } from "../helpers";
+
 const Register = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+  const { signUp, loading } = useContext(AuthContext);
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [passwordErrors, setPasswordErrors] = useState({
+    length: false,
+    capital: false,
+    specialCharacter: false,
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+
+    // Password validation
+    if (name === "password") {
+      const errors = {
+        length: value.length < 6,
+        capital: !/[A-Z]/.test(value),
+        specialCharacter: !/[!@#$%^&*(),.?":{}|<>]/.test(value),
+      };
+
+      setPasswordErrors(errors);
+    }
+  };
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const { email, password } = formData;
+
+    if (
+      passwordErrors.length ||
+      passwordErrors.capital ||
+      passwordErrors.specialCharacter
+    ) {
+      toast.error("Invalid password. Please check the password requirements.");
+      return;
+    }
+
+    signUp(email, password)
+      .then((result) => {
+        console.log(result.user);
+        toast.success("Account created successfully!");
+        setTimeout(() => {
+          navigate(from, { replace: true });
+        }, 1500);
+      })
+      .catch((error) => {
+        const errorMessage = formatFirebaseAuthErrorMessage(error);
+        toast.error(errorMessage);
+      });
+
+    // Reset the form
+    setFormData({
+      email: "",
+      password: "",
+    });
+    setPasswordErrors({
+      length: false,
+      capital: false,
+      specialCharacter: false,
+    });
+  };
+
   return (
-    <div>
-      <h1 className="text-4xl">This Register Page</h1>
+    <div className="bg-gradient-to-r from-pink-400 via-purple-300 to-pink-400 py-20 ">
+      <div className="w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md">
+        <div className="px-6 py-4">
+          <div >
+            <h1 className="text-lg text-center text-pink-600">Do'nt Have An Account?</h1>
+          </div>
+
+          <h3 className="mt-3 text-4xl font-bold text-pink-600 text-center">
+            Register
+          </h3>
+
+          <form onSubmit={handleRegister}>
+            <div className="w-full mt-4">
+              <h3 className="text-gray-600">Email</h3>
+              <input
+                className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+                type="text"
+                name="email"
+                id="email"
+                placeholder="example@gmail.com"
+                value={formData.email}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div className="w-full mt-4">
+              <h3 className="text-gray-600">Password</h3>
+              <input
+                className={`block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300 ${
+                  passwordErrors.length ||
+                  passwordErrors.capital ||
+                  passwordErrors.specialCharacter
+                    ? "border-red-500"
+                    : ""
+                }`}
+                type="password"
+                name="password"
+                id="password"
+                placeholder="•••••••••"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
+              <ul className="list-disc list-inside mt-3 space-y-2">
+                {passwordErrors.length && (
+                  <li className="text-red-500 text-xs">
+                    Password must be at least 6 characters.
+                  </li>
+                )}
+                {passwordErrors.capital && (
+                  <li className="text-red-500 text-xs">
+                    Include at least one capital letter.
+                  </li>
+                )}
+                {passwordErrors.specialCharacter && (
+                  <li className="text-red-500 text-xs">
+                    Include at least one special character.
+                  </li>
+                )}
+              </ul>
+            </div>
+
+            <div className="flex items-center justify-end mt-4">
+            <button
+            disabled={loading}
+              type="submit"
+              className="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-pink-700 rounded-lg hover:bg-purple-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50 disabled:bg-purple-700/50"
+            >
+              {
+                loading ? "Loading" : "Register"
+              }
+              
+            </button>
+            </div>
+          </form>
+          <p className="mt-4 text-sm font-light text-center text-gray-500">
+            {" "}
+            Already have an account?{" "}
+            <Link
+              to="/login"
+              className="font-medium text-pink-700 hover:underline"
+            >
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
